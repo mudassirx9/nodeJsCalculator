55422817aa0604282b12acddf71d665f4f236505
mudassirx9 <mudassir.hussain.529@gmail.com>
2020-11-10 16:05:02 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app =express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.use('/assets',express.static('assets'))
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.")
+})
+
+
